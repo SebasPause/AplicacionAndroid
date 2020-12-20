cce31a7c802e9c6597244b5ed40245b10ec8eae0
Sebastian <adriansebastian.pauseniuc@iesvallecidacos.com>
2020-12-20 21:44:13 +0100

    Diseño

--- app/src/main/java/com/example/estilosapp/Vista/ContentMainActivity.java
+++ app/src/main/java/com/example/estilosapp/Vista/ContentMainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.estilosapp.R;
+
 public class ContentMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
     }
+
 }
--- app/src/main/java/com/example/estilosapp/Vista/PerfilActivity.java
+++ app/src/main/java/com/example/estilosapp/Vista/PerfilActivity.java
@@ -1,13 +1,42 @@
 package com.example.estilosapp.Vista;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.estilosapp.R;
 
 public class PerfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_perfil,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.btnEditarPerfil){
+            lanzarEditarPerfil(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void lanzarEditarPerfil(View view) {
+        Intent intent = new Intent(this,EditarPerfilActivity.class);
+        startActivity(intent);
     }
 }
